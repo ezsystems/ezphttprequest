2a0e6e247c464360823fb3f4e861e72f6bd6b449
Bertrand Dunogier <bd@ez.no>
2012-05-28 10:36:14 +0200

    Added ezpHttpRequest class

---
+++ classes/ezphttprequest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * File containing the eZRecommendationException class.
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+class ezpHttpRequest extends HttpRequest
+{
+    public function __construct( $url, $requestMethod = HTTP_METH_GET, array $options = array() )
+    {
+	parent::__construct( $url, $requestMethod, $options );
+    }
+}
+?>
