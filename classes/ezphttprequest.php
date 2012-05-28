e7041f4175c22aeba51f379ec40ebc013d033aa0
Bertrand Dunogier <bd@ez.no>
2012-05-28 13:38:13 +0200

    Implemented Proxy based on site.ini settings

--- classes/ezphttprequest.php
+++ classes/ezphttprequest.php
@@ -11,7 +11,32 @@ class ezpHttpRequest extends HttpRequest
 {
     public function __construct( $url, $requestMethod = HTTP_METH_GET, array $options = array() )
     {
-	parent::__construct( $url, $requestMethod, $options );
+        static $proxyOptions = array(), $hasProxyOptions = false;
+
+        // Proxy settings from site.ini
+        if ( !$hasProxyOptions )
+        {
+            $ini = eZINI::instance();
+            $proxyServer = $ini->hasVariable( 'ProxySettings', 'ProxyServer' ) ? $ini->variable( 'ProxySettings', 'ProxyServer' ) : false;
+
+            if ( $proxyServer )
+            {
+                $proxyServerParts = parse_url( $proxyServer );
+                $proxyOptions['proxyhost'] = $proxyServerParts['host'];
+                $proxyOptions['proxyport'] = $proxyServerParts['port'];
+                $proxyOptions['proxytype'] = HTTP_PROXY_HTTP;
+                $proxyUsername = $ini->hasVariable( 'ProxySettings', 'User' ) ? $ini->variable( 'ProxySettings', 'User' ) : false;
+                $proxyPassword = $ini->hasVariable( 'ProxySettings', 'Password' ) ? $ini->variable( 'ProxySettings', 'Password' ) : false;
+                if ( $proxyUsername )
+                {
+                    $proxyOptions['proxyauth'] = "$proxyUsername:$proxyPassword";
+                }
+            }
+
+            $hasProxyOptions = true;
+        }
+
+        parent::__construct( $url, $requestMethod, $options + $proxyOptions );
     }
 }
 ?>
